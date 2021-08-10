617fbda45c5e900c3e6b4f731b79826d1b19f1b3
Robin <playlist.robin@gmail.com>
2021-08-10 10:37:05 +0200

    MaJ Design stat
    Correction non finie de a page produit
    modification de laccueil et ajout des ressources

---
+++ frontend/src/data.js
@@ -0,0 +1,25 @@
+export default{
+    products : [
+        {
+            _id:'1',
+            name:'',
+            price:'',
+            description:'',
+            imageURL:'',
+        },
+        {
+            _id:'2',
+            name:'',
+            price:'',
+            description:'',
+            imageURL:'',
+        },
+        {
+            _id:'3',
+            name:'',
+            price:'',
+            description:'',
+            imageURL:'',
+        },
+    ]
+}
