efba4c3ab351638f507067224da08a2f108d8926
Robin <playlist.robin@gmail.com>
2021-09-07 14:13:53 +0200

    Maj index.js
    creation de index.js
    insertion de index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,38 @@
+fetch('http://localhost:3000/api/teddies')
+    .then((response) => response.json())
+    .then((response) =>{
+
+            console.log(response);
+
+        let getProducts = "";
+
+        for(let i = 0; i<response.length; i++){
+
+            console.log(response[i].name);
+
+
+        getProducts += 
+        `<li class="item">
+            <div class="card mb-3">
+                <div id="cardProduct" class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${response[i].imageUrl }" class="img-thumbnail shadow" alt="Ours" />
+                    </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h2 class="card-title fs-4">${response[i].name}</h2>
+                                <p class="card-text">${response[i].description}</p>
+                                <p class="card-text text-muted fs-5">${(response[i].price/100).toFixed(2).replace(".",",")}€ </p>
+                                <br />
+                                <a class="btn btn-outline-primary w-25" href="page_produitALT.html?${response[i]._id}">Voir Produit</a>
+                            </div>
+                        </div>
+                    </div>
+                 </div>
+            </li>`
+        }
+        
+        document.getElementById("items").innerHTML = getProducts
+    })
+
+
